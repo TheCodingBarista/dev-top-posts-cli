5ef62b088f7a5c24d3ac11391127e6fbda33de43
melissa_holt <melissa.holt21@gmail.com>
2020-01-13 23:46:11 -0800

    Defined get_users method

--- lib/top_posts/api_service.rb
+++ lib/top_posts/api_service.rb
@@ -4,12 +4,13 @@ class TopPosts::ApiService
         url = "https://dev.to/api/articles?key=z2syANA9RkwS29rT86aAg3q2"
         response = RestClient.get(url)
         page = (JSON.parse(response))
+        binding.pry
         articles = []
 
         page.each do |article|
             title = article["title"].gsub(/"/, '')
             description = article["description"]
-               if  description.size == 0
+                if  description.size == 0
                     description = "This article has no description."
                 end
             url = article["url"]
@@ -27,34 +28,28 @@ class TopPosts::ApiService
     end
 
     def self.get_users
-        def self.get_articles
-            url = "https://dev.to/api/articles?key=z2syANA9RkwS29rT86aAg3q2"
-            response = RestClient.get(url)
-            page = (JSON.parse(response))
-            articles = []
-    
-            page.each do |article|
-                title = article["title"].gsub(/"/, '')
-                description = article["description"]
-                   if  description.size == 0
-                        description = "This article has no description."
-                    end
-                url = article["url"]
-                tags = article["tag_list"]
-                
-                article_hash = {
-                    :title => title,
-                    :description => description,
-                    :url => url,
-                    :tags => tags
-                }
-            articles << article_hash
-            end
-            articles
+        url = "https://dev.to/api/articles?key=z2syANA9RkwS29rT86aAg3q2"
+        response = RestClient.get(url)
+        page = (JSON.parse(response))
+        users = []
+
+        page.each do |article|
+            user = article["user"]
+               name = user["name"]
+               username = user["username"]
+               website_url = user["website_url"]
+                if website_url == nil
+                    website_url = "This user does not have a website."
+                end
+            
+                user_hash = {
+                    :name => name,
+                    :username => username,
+                    :website_url => website_url
+             }
+        users << user_hash
         end
+        users
     end
-
-
-  
 
 end
--- lib/top_posts/article.rb
+++ lib/top_posts/article.rb
@@ -1,5 +1,5 @@
 class TopPosts::Article
-   attr_accessor :title, :id, :url, :publish_date, :tags, :tag_list
+   attr_accessor :title, :description, :url, :tags
    @@all = []
 
    #def initialize(user, attributes)
@@ -11,14 +11,8 @@ class TopPosts::Article
 
    #end
 
-   def initialize(args)
+   def initialize
       
-      @title = args[0]["title"]
-      @id = args[0]["id"]
-      @url = args[0]["url"]
-      @publish_date = args[0]["published_at"]
-      @tags = args[0]["tags"]
-      @tag_list = args[0]["tag_list"]
    end
 
    def self.all
@@ -26,9 +20,7 @@ class TopPosts::Article
    end
 
    def self.print_all
-      all.each_with_index do |article, index|
-         puts "#{index+1}.   #{article.title}" 
-      end
+
    end
 
 end
